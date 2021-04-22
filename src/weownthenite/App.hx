23dc56e2270819f6de41c76e93a6a5df4968edef
tong <tong@disktree.net>
2021-04-22 10:11:39 +0200

    ...

--- src/weownthenite/App.hx
+++ src/weownthenite/App.hx
@@ -1,5 +1,6 @@
 package weownthenite;
 
+import js.html.Notification;
 import js.Browser.console;
 import js.Browser.document;
 import js.Browser.window;
@@ -14,6 +15,7 @@ using om.ArrayTools;
 final RTMP_HOST = "vvv.disktree.net";
 final RTMP_PORT = 1935;
 final RTMP_APP = "weownthenite";
+
 final HSL_HOST = "vvv.disktree.net";
 final STREAM = "live";
 
@@ -22,7 +24,7 @@ var VIDEOS = weownthenite.Build.getVideoFiles('web/video/1920');
 var video : VideoElement;
 var index : Int;
 var live = true;
-var hls : Hls;
+//var hls : Hls;
 
 function playNextVideo() {
 	if( live ) return;
@@ -31,6 +33,7 @@ function playNextVideo() {
 }
 
 function playVideo( i : Int ) {
+	trace("playVideo",i,live);
 	if( live ) return;
 	index = i;
 	//video.pause();
@@ -67,6 +70,7 @@ function playVideo( i : Int ) {
 	video.play();
 }
 
+/*
 function initLivePlayback() : Promise<Dynamic> {
 	
 	if( !Hls.isSupported() )
@@ -101,6 +105,25 @@ function initLivePlayback() : Promise<Dynamic> {
 		hls.attachMedia(video);
 	});
 }
+*/
+
+/*
+function initLivePlayback()  {
+	var url = 'https://$HSL_HOST/$RTMP_APP/$STREAM.m3u8';
+	hls = new Hls();
+	hls.on( MANIFEST_PARSED, (event,data) -> {
+		trace('manifest loaded, found ' + data.levels.length + ' quality level');
+		// resolve( null );
+		hls.attachMedia(video);
+	});
+	hls.on( MEDIA_ATTACHED, () -> {
+		trace('video and hls.js bound together');
+		//video.muted = true;
+		video.play();
+	});
+	hls.on( ERROR, handleHslError );
+	hls.loadSource(url);
+}
 
 function initStaticPlayback() {
 	VIDEOS.shuffle();
@@ -110,6 +133,13 @@ function initStaticPlayback() {
 	video.onclick = playNextVideo;
 }
 
+function handleHslError( e, data ) {
+	trace(e);
+	live = false;
+	initStaticPlayback();
+}
+*/
+
 function main() {
 	
 	window.addEventListener( 'load', e -> {
@@ -118,25 +148,116 @@ function main() {
 		
 		final header = document.body.querySelector('header');
 		final footer = document.body.querySelector('footer');
-	
-		video = cast document.getElementById('video');
 		
-		window.oncontextmenu = e -> {
-			e.preventDefault();
-			return false;
-		}
+		// var infoVolume = document.createDivElement();
+		// infoVolume.textContent = '99';
+		// footer.append( infoVolume );
 		
 		var liveLink = document.createAnchorElement();
 		liveLink.textContent = 'LIVE';
-		liveLink.classList.add( 'live' );
+		liveLink.title = 'BROADCAST OFFLINE';
+		liveLink.classList.add( 'live', 'unavailable' );
 		footer.append( liveLink );
 		
-		var mailLink = document.createAnchorElement();
+		var src = 'https://$HSL_HOST/$RTMP_APP/$STREAM.m3u8';
+		
+		video = cast document.getElementById('video');
+		
+		/* var mailLink = document.createAnchorElement();
 		mailLink.textContent = 'MAIL';
 		mailLink.href = "mailto:hello@weownthenite.org";
 		mailLink.title = "⸸";
-		footer.append( mailLink );
+		footer.append( mailLink ); */
 		
+		/* video.onloadstart = e -> trace(e);
+		video.onplay = e -> {
+			trace(e);
+			if( live ) {
+				console.info( 'WE⸸ARE⸸LIVE' );
+			}
+		} */
+
+		video.onplay = e -> {
+			if( live ) {
+				console.info( 'WE⸸ARE⸸LIVE' );
+				liveLink.classList.remove( 'unavailable' );
+				liveLink.title = '$src';
+			} else {
+				liveLink.classList.add( 'unavailable' );
+				liveLink.title = 'BROADCAST OFFLINE';
+			}
+		}
+
+		video.onerror = e -> console.warn(e);
+
+		/* video.onclick = e -> {
+			//if( !live ) playNextVideo();
+			video.paused ? video.play() : video.pause();
+		} */
+
+		live = true;
+		if( video.canPlayType('application/vnd.apple.mpegurl') != "" ) {
+			video.src = src;
+		} else {
+			var hls = new Hls();
+			hls.on( MANIFEST_PARSED, (event,data) -> {
+				trace('manifest loaded, found ' + data.levels.length + ' quality level');
+				hls.attachMedia(video);
+			});
+			hls.on( MEDIA_ATTACHED, (e,data) -> {
+				trace('video and hls.js bound together');
+				video.onclick = video.play;
+			});
+			hls.on( ERROR, (e,d) -> {
+				trace(e);
+				live = false;
+				hls.destroy();
+				video.autoplay = true;
+				//video.preload = "all";
+				video.onclick = playNextVideo;
+				video.onended = playNextVideo;
+				///playNextVideo();
+			});
+			hls.loadSource(src);
+		}
+/* 		untyped Notification.requestPermission().then(function(result) {
+			console.log(result);
+			var img = '/to-do-notifications/img/icon-128.png';
+			var text = 'HEY! Your task "' + "666" + '" is now overdue.';
+			var notification = new Notification('To do list', { body: text, icon: img });
+		}); */
+		
+		/*
+		video.canPlayType('application/vnd.apple.mpegurl')) {
+			video.src = src;
+		}
+ */
+
+			/*
+			hls.on( MANIFEST_PARSED, (event,data) -> {
+				trace('manifest loaded, found ' + data.levels.length + ' quality level');
+				// resolve( null );
+			});
+			hls.on( ERROR, (e,data) -> {
+				trace(e);
+				if( data.fatal ) {
+					trace("FATAL!");
+					hls.destroy();
+					// reject(e);
+					//hls = null;
+				}
+			});
+			hls.on( MEDIA_ATTACHED, (e,data) -> {
+				trace('video and hls.js bound together');
+				hls.loadSource(src);
+			});
+			hls.on( DESTROYING, (e,data) -> {
+				trace(e);
+			});
+			hls.attachMedia(video);
+			*/
+
+			/*
 		initLivePlayback().then( r  -> {
 			console.info( 'WE⸸ARE⸸LIVE' );
 			live = true;
@@ -156,12 +277,31 @@ function main() {
 	
 			initStaticPlayback();
 		});
+		*/
 	
+		window.onwheel = e -> {
+			console.debug(e);
+			if( e.deltaY > 0 ) {
+				var v = video.volume - 0.1; 
+				if( v < 0 ) v = 0;
+				video.volume = v; 
+			} else {
+				var v = video.volume + 0.1; 
+				if( v > 1.0 ) v = 1;
+				video.volume = v; 
+			}
+		}
+
 		window.onkeydown = e -> switch e.key {
 			case " ":
 				if( !live ) playNextVideo();
 			case _:
 				trace(e);
+		}
+
+		window.oncontextmenu = e -> {
+			e.preventDefault();
+			return false;
 		}
 	});
 }
