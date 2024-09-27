e72ecd71406fdc176aa8381d8291ef3904e5e0ed
tong <tong@disktree.net>
2024-09-27 14:48:36 +0200

    initial react version (v3)

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import VideoPlayer from './VideoPlayer'
+
+export default function App() {
+  return <VideoPlayer></VideoPlayer>
+}
+
---
+++ src/VideoPlayer.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useRef, useState } from 'react'
+import { shuffleArray } from './utils'
+
+const VIDEO_RES = 1920;
+
+const PLAYLIST = [
+  "bikeball-15.mp4",
+  "cant.mp4",
+  "haarp-2.mp4",
+  "harga.mp4",
+  "maybetomorrow.mp4",
+  "nottoday-4.mp4",
+  "nottoday.mp4",
+  "panzerpalme.mp4",
+  "panzerschokolade-12.mp4",
+  "panzerschokolade-19.mp4",
+  "panzerschokolade-20.2.mp4",
+  "panzerschokolade-20.3.mp4",
+  "panzerschokolade-20.4.mp4",
+  "panzerschokolade-20.5.mp4",
+  "panzerschokolade-20.6.mp4",
+  "panzerschokolade-20.7.mp4",
+  "panzerschokolade-20.mp4",
+  "panzerschokolade-4.mp4",
+  "panzerschokolade-5.mp4",
+  "panzerschokolade-tropicana-1.mp4",
+  "panzerschokolade-tropicana-2.mp4",
+  "panzerschokolade-tropicana-3.mp4",
+  "sierpinski.mp4",
+  "tekktonic.mp4",
+  "wkr-1.mp4",
+];
+
+// TODO: responsive video resolution
+// TODO: hls live streaming
+
+export default function VideoPlayer() {
+
+  const [videoIndex, setVideoIndex] = useState(0);
+  const [playlist, _setPlaylist] = useState(() => shuffleArray(PLAYLIST));
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const videoPreloadRef = useRef<HTMLVideoElement>(null)
+  //let videoRes = 1920;
+
+  function getNextVideoIndex(currentIndex?: number) {
+    if (currentIndex === undefined) currentIndex = videoIndex;
+    let id = currentIndex + 1;
+    if (id == PLAYLIST.length) id = 0;
+    return id
+  }
+
+  function getVideoUrl(index: number): string {
+    return `video/${VIDEO_RES}/${playlist[index]}`
+  }
+
+  function playNext() {
+    const nextIndex = getNextVideoIndex();
+    setVideoIndex(nextIndex)
+    if (videoRef.current !== null) {
+      videoRef.current.muted = false;
+      videoRef.current.src = getVideoUrl(nextIndex);
+    }
+    if (videoPreloadRef.current !== null) {
+      const i = getNextVideoIndex(nextIndex);
+      videoPreloadRef.current.src = getVideoUrl(i);
+    }
+  }
+
+  function handleVideoEnd() {
+    //console.log('video ended')
+    playNext();
+  }
+
+  function handleMouseDown() {
+    playNext()
+  }
+
+  function handleKeyDown(e: KeyboardEvent) {
+    switch (e.code) {
+      case "Space":
+      case "Enter":
+        playNext()
+    }
+  }
+
+  useEffect(() => {
+    if (videoRef.current !== null) {
+      videoRef.current.addEventListener("ended", handleVideoEnd);
+    }
+    document.body.oncontextmenu = e => { e.preventDefault() }
+    document.body.addEventListener("click", handleMouseDown, false)
+    window.addEventListener('keypress', handleKeyDown, false);
+    const vref = videoRef.current;
+    return () => {
+      if (vref !== null) {
+        vref.removeEventListener("ended", handleVideoEnd);
+      }
+      document.body.oncontextmenu = null;
+      document.body.removeEventListener("click", handleMouseDown, false)
+      window.removeEventListener('keypress', handleKeyDown);
+    }
+  })
+
+  return (
+    <div>
+      <video id="video-player" autoPlay muted ref={videoRef}></video>
+      <video id="video-preload" autoPlay muted ref={videoPreloadRef}></video>
+    </div>
+  )
+}
+
